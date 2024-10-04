fd52e1bf36e1f372e4cf02502a9d49ef8256aa85
HenriqueSaniW <108810733+HenriqueSaniW@users.noreply.github.com>
2024-10-03 21:38:35 -0300

    page about

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,12 +1,24 @@
-import '../../styles/Sobre/sobre.css'
+import '../../styles/Sobre/Sobre.css'
 import NavBar from '../../components/Navbar/Navbar';
 
 function Sobre() {
     return (
         <div className="sobre-container">
-            
-            <NavBar />
+            <div className='nav-bar'>
 
+            <NavBar />
+            </div>
+            <section className='container-low'>
+                <div className='container-conteudo'>
+                    
+                <div style={{background: 'red'}}>
+
+                </div>
+                <div style={{background: 'blue'}}>
+
+                </div>
+                </div>
+            </section>
         </div>
     )
 }
