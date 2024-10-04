8fb6523c9ae4fd7d10872a5717e757bcce57c665
Ivan Ramos Biagioni <140509979+ivan-maze@users.noreply.github.com>
2024-10-03 21:32:20 -0300

    starting cards

--- src/pages/Catalogo/Catalogo.jsx
+++ src/pages/Catalogo/Catalogo.jsx
@@ -1,12 +1,63 @@
 import "../../styles/Catalogo/catalogo.css"
 import NavBar from "../../components/Navbar/Navbar";
+import {Link} from 'react-router-dom'
+
 
 function Catalogo() {
     return (
         <div className="catalogo-container">
-            
-            <NavBar />
+            <div className="nav-bar">
+                <NavBar/>
+            </div>
+            <div className="catalogo-content">
+                <div className="card">
+                    <div className="titulo">
+                        <h3>Triticum aestivum (Trigo Resiliente)</h3>
+                    </div>
+                    <div className="corpo-a">
+                        <ul className="catalogo-card">
+                            <li>Fenótipo: Tolerância à seca, altura média, folhas estreitas.</li>
+                            <li>Local: Região semiárida do Nordeste brasileiro.</li>
+                            <li>Temperatura média: 28°C</li>
+                            <li>Solo: Solo arenoso e pobre em nutrientes.</li>
+                            <li>Clima: Semiárido, baixa pluviosidade, altos níveis de radiação solar.</li>
+                            <li>Observações: Esta variedade de trigo tem alta resiliência à seca prolongada e consegue completar seu ciclo de vida com pouca água.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="titulo">
+                        <h3>Oryza sativa (Arroz Tolerante à Salinidade)</h3>
+                    </div>
+                    <div className="corpo-b">
+                        <ul className="catalogo-card">
+                            <li>Fenótipo: Plantas de porte médio, grãos médios e resistentes ao sal.</li>
+                            <li>Local: Delta do Mekong, Vietnã.</li>
+                            <li>Temperatura média: 26°C</li>
+                            <li>Solo: Solo salino em áreas costeiras.</li>
+                            <li>Clima: Tropical úmido, altas taxas de precipitação.</li>
+                            <li>Observações: Esse arroz é altamente tolerante a solos salinos e possui boa produtividade em áreas sujeitas à intrusão de água do mar, sendo uma opção estratégica para áreas costeiras afetadas por mudanças climáticas.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="titulo">
+                        <h3>Zea mays (Milho Tolerante ao Frio)</h3>
+                    </div>
+                    <div className="corpo-c">
+                        <ul lassName="catalogo-card">
+                            <li>Fenótipo: Estatura baixa, folhas largas e resistentes a geadas leves.</li>
+                            <li>Local: Altiplano Andino, Bolívia.</li>
+                            <li>Temperatura média: 12°C</li>
+                            <li>Solo: Solo argiloso, rico em minerais.</li>
+                            <li>Clima: Frio e seco, com grande variação de temperatura entre o dia e a noite.</li>
+                            <li>Observações: Esta variedade de milho é adaptada a climas frios e pode sobreviver a geadas noturnas leves. Seu ciclo de crescimento é mais longo devido às temperaturas baixas, mas é ideal para regiões montanhosas.</li>
+                        </ul>
+                    </div>
+                </div>
+         
 
+            </div>
         </div>
     )
 }
