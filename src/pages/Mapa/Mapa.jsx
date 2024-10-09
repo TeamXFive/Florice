ef9a47229bbe1fd4c3c4697531c178a45a2c80df
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-10-08 23:16:49 -0300

    [feat] Bubble map + slightly visual changes
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- src/pages/Mapa/Mapa.jsx
+++ src/pages/Mapa/Mapa.jsx
@@ -1,10 +1,22 @@
+import NavBar from "../../components/Navbar/Navbar";
 import "../../styles/Mapa/Mapa.css";
 
 function Mapa() {
     return (
-        <>
-            <span>Mapa vem aqui hihihi</span>
-        </>
+        <article className="mapa-container">
+            <section className="mapa-navbar">
+                <NavBar />
+            </section>
+
+            <section className="looker-container">
+                <iframe
+                    className="embed-iframe"
+                    src="https://lookerstudio.google.com/embed/reporting/54a3d92e-94f3-42e5-b2c9-3aa450a63b5a/page/jLkEE"
+                    allowfullscreen
+                    sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+                ></iframe>
+            </section>
+        </article>
     );
 }
 
--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,35 +1,39 @@
-import '../../styles/Sobre/Sobre.css'
-import NavBar from '../../components/Navbar/Navbar';
+import "../../styles/Sobre/sobre.css";
+import NavBar from "../../components/Navbar/Navbar";
 
 function Sobre() {
     return (
         <div className="sobre-container">
-            <div className='nav-bar'>
-
-            <NavBar />
+            <div className="nav-bar">
+                <NavBar />
             </div>
-            <section className='container-low'>
-                <div className='container-conteudo'>
-                <div className='foto-container'>
-                    <div className='detalhe-box'>
+            <section className="container-low">
+                <div className="container-conteudo">
+                    <div className="foto-container">
+                        <div className="detalhe-box"></div>
+                        <div className="foto-1"></div>
+                        <div className="foto-2"></div>
                     </div>
-                    <div className='foto-1'></div>
-                    <div className='foto-2'></div>
-                </div>
-                <div className='texto-container'>
-                    <div className='texto-conteudo'>
-                O Florice é um projeto que visa facilitar a catalogação online de bancos de germoplasmas,
-                 lugares pensados para a proteção desses germoplasmas, isso porque em um banco de germoplasma são armazenados
-                  materiais genéticos de plantas estrategicamente selecionadas, mirando na escolha de genes que sobrevivem
-                   à situações intensas (provocadas, por exemplo, pelas crises climáticas). Assim, o Florice
-                    se torna uma fonte direta para pesquisadores, agricultores e produtores rurais que se asseguram que a 
-                    diversidade gênica chegue na sociedade.
+                    <div className="texto-container">
+                        <div className="texto-conteudo">
+                            O Florice é um projeto que visa facilitar a
+                            catalogação online de bancos de germoplasmas,
+                            lugares pensados para a proteção desses
+                            germoplasmas, isso porque em um banco de germoplasma
+                            são armazenados materiais genéticos de plantas
+                            estrategicamente selecionadas, mirando na escolha de
+                            genes que sobrevivem à situações intensas
+                            (provocadas, por exemplo, pelas crises climáticas).
+                            Assim, o Florice se torna uma fonte direta para
+                            pesquisadores, agricultores e produtores rurais que
+                            se asseguram que a diversidade gênica chegue na
+                            sociedade.
+                        </div>
                     </div>
-                </div>
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
 export default Sobre;
