0fe60d3f3b959aa448525b765acd6664b871a517
Jose Othon <othon200@gmail.com>
2024-11-03 10:35:41 -0300

    feat: add API CORS headers, FE leaflet and fetch places

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import "./styles/App.css";
 import NavBar from "./components/Navbar/Navbar";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Sobre from "./pages/Sobre/Sobre";
 import Catalogo from "./pages/Catalogo/Catalogo";
-import Mapa from "./pages/Mapa/Mapa";
+import { Mapa } from "./pages/Mapa/Mapa";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/Signup";
-import { createPortal } from "react-dom";
-import { useEffect, useRef } from "react";
 
 function App() {
-    const lookerContainer = useRef();
-
-    const path = useLocation().pathname;
-
-    useEffect(() => {
-        const ref = document.querySelector(".looker-container");
-        if (ref) {
-            lookerContainer.current = ref;
-        }
-    }, [path]);
-
     return (
         <div className="container-app">
             <NavBar />
@@ -36,15 +23,6 @@ function App() {
                     <Route path="/signup" element={<SignUp />} />
                 </Routes>
             </main>
-            {createPortal(
-                <iframe
-                    className="looker-iframe embed-iframe"
-                    src="https://lookerstudio.google.com/embed/reporting/54a3d92e-94f3-42e5-b2c9-3aa450a63b5a/page/jLkEE"
-                    allowfullscreen
-                    sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
-                ></iframe>,
-                lookerContainer.current || document.body
-            )}
         </div>
     );
 }
